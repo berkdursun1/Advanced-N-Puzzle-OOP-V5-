438bd5a1c0bed7734c5f302981e8459d01ecf3a3
berkdursun1 <43707798+berkdursun1@users.noreply.github.com>
2020-08-10 16:19:38 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<fstream>
+#include<vector>
+#define empty 0
+using namespace std;
+
+#include "main.h"
+int main(int argc, char* argv[]){
+	
+	test_function();
+	return 0;
+}
